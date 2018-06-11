0e9a0784a9f5fe9f9c6fcb14a18d557ab202ff07
Eric Fiorani <efiorani305@gmail.com>
2018-06-11 21:05:21 +0000

    Introduction and Workout article now operational

--- lib/daily_fitness/cli.rb
+++ lib/daily_fitness/cli.rb
@@ -12,28 +12,30 @@ class DailyFitness::CLI
     puts "Hello! Are you ready to supplement your fitness lifestyle?".colorize(:green) + "
     \nChoose between three options.
     Press “1” for the Nutritional Article of the day,
-    press “2” for the Workout Article of the day,
-    press “3” for the Supplement Article of the day,
-    press “4” for the Motivational Article of the day,
-    or type 'exit' if you do not want to continue"
+    Press “2” for the Workout Article of the day,
+    Press “3” for the Supplement Article of the day,
+    Press “4” for the Motivational Article of the day,
+    Or type 'exit' if you do not want to continue"
   end
 
   def workout_article
     @article = DailyFitness::Workout.info
     @article.each do |article|
       puts "------------------------"
-      puts "#{article.title}".colorize(:green) + "\n\n#{article.description}" + "#{article.url}".colorize(:light_blue)
-      puts "------------------------"
+      puts "\n#{article.title}".colorize(:green) + "\n\n#{article.description}" +
+      "\n\nClick on the link to view the full report -" + " #{article.url}".colorize(:light_blue)
+      puts "\n------------------------"
     end
     list
   end
 
   def list
-    puts "\nPress “1” for the Nutritional Article of the day,
-    press “2” for the Workout Article of the day,
-    press “3” for the Supplement Article of the day,
-    press “4” for the Motivational Article of the day,
-    or type 'exit' if you do not want to continue"
+    puts"
+    Press “1” for the Nutritional Article of the day,
+    Press “2” for the Workout Article of the day,
+    Press “3” for the Supplement Article of the day,
+    Press “4” for the Motivational Article of the day,
+    Or type 'exit' if you do not want to continue"
   end
 
   def nutritional_article
@@ -52,7 +54,8 @@ class DailyFitness::CLI
   end
 
   def choose
-    puts "\nPlease proceed with one of the options above if you are serious about your gains! \nIf you do not want to continue, just type 'exit'."
+    puts "\nPlease proceed with one of the options above if you are serious about your gains!".colorize(:green) +
+    " \nIf you do not want to continue, just type 'exit'."
     input = nil
     while input != "exit"
       # puts "\n Would you like to choose another option?"
