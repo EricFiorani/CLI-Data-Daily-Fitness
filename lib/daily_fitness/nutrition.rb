2d1dea521886bc8c29edc03b87bae7c76e3b7b9e
Eric Fiorani <35820351+EricFiorani@users.noreply.github.com>
2018-06-13 17:08:37 -0400

    Added sub Nutrition articles to Class

--- lib/daily_fitness/nutrition.rb
+++ lib/daily_fitness/nutrition.rb
@@ -9,12 +9,15 @@ class DailyFitness::Nutrition
   def self.scrape_article
     article = [] #Creates an empty array of the article
 
-    article << self.bodybuilding #Adds the scraped data to the article array
+    article << self.featured #Adds the scraped data to the article array
+    article << self.sub_article_1
+    article << self.sub_article_2
+    article << self.sub_article_3
 
     article #Calls on the array to produce the data.
   end
 
-  def self.bodybuilding
+  def self.featured
 
     doc = Nokogiri::HTML(open("https://bodybuilding.com"))
 
@@ -22,6 +25,45 @@ class DailyFitness::Nutrition
     article.title = doc.search("#DPG_Nutrition_Main .cms-article-list--article.hero .title").text.strip
     article.description = doc.search("#DPG_Nutrition_Main .cms-article-list--article.hero .description").text.strip
     article.url = doc.search("#DPG_Nutrition_Main .cms-article-list--article.hero a.thumb-container").map {|link| link['href'] }
+
+    # binding.pry
+    article
+  end
+
+  def self.sub_article_1
+
+    doc = Nokogiri::HTML(open("https://bodybuilding.com"))
+
+    article = self.new
+    article.title = doc.search("#DPG_Nutrition_Sub_1 .title").text.strip
+    article.description = doc.search("#DPG_Nutrition_Sub_1 .description").text.strip
+    article.url = doc.search("#DPG_Nutrition_Sub_1 a.thumb-container").map {|link| link['href'] }
+
+    # binding.pry
+    article
+  end
+
+  def self.sub_article_2
+
+    doc = Nokogiri::HTML(open("https://bodybuilding.com"))
+
+    article = self.new
+    article.title = doc.search("#DPG_Nutrition_Sub_2 .title").text.strip
+    article.description = doc.search("#DPG_Nutrition_Sub_2 .description").text.strip
+    article.url = doc.search("#DPG_Nutrition_Sub_2 a.thumb-container").map {|link| link['href'] }
+
+    # binding.pry
+    article
+  end
+
+  def self.sub_article_3
+
+    doc = Nokogiri::HTML(open("https://bodybuilding.com"))
+
+    article = self.new
+    article.title = doc.search("#DPG_Nutrition_Sub_3 .title").text.strip
+    article.description = doc.search("#DPG_Nutrition_Sub_3 .description").text.strip
+    article.url = doc.search("#DPG_Nutrition_Sub_3 a.thumb-container").map {|link| link['href'] }
 
     # binding.pry
     article
