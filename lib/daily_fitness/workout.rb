6da92028d803c1ff10ac8c3ad225920f667c3b57
Eric Fiorani <35820351+EricFiorani@users.noreply.github.com>
2018-06-13 17:01:01 -0400

    Additional Articles for Workout Class

--- lib/daily_fitness/workout.rb
+++ lib/daily_fitness/workout.rb
@@ -10,6 +10,9 @@ class DailyFitness::Workout
     article = [] #Creates an empty array of the article
 
     article << self.bodybuilding #Adds the scraped data to the article array
+    article << self.sub_article_1
+    article << self.sub_article_2
+    article << self.sub_article_3
 
     article #Calls on the array to produce the data.
   end
@@ -23,8 +26,48 @@ class DailyFitness::Workout
     article.description = doc.search("#DPG_Workouts_Main .cms-article-list--article.hero .description").text.strip
     article.url = doc.search("#DPG_Workouts_Main .cms-article-list--article.hero a.thumb-container").map {|link| link['href'] }
 
+  # binding.pry
+    article
+  end
+
+  def self.sub_article_1
+
+    doc = Nokogiri::HTML(open("https://bodybuilding.com"))
+
+    article = self.new
+    article.title = doc.search("#DPG_Workouts_Sub_1 .title").text.strip
+    article.description = doc.search("#DPG_Workouts_Sub_1 .description").text.strip
+    article.url = doc.search("#DPG_Workouts_Sub_1 a.thumb-container").map {|link| link['href'] }
+
     # binding.pry
     article
   end
+
+  def self.sub_article_2
+
+    doc = Nokogiri::HTML(open("https://bodybuilding.com"))
+
+    article = self.new
+    article.title = doc.search("#DPG_Workouts_Sub_2 .title").text.strip
+    article.description = doc.search("#DPG_Workouts_Sub_2 .description").text.strip
+    article.url = doc.search("#DPG_Workouts_Sub_2 a.thumb-container").map {|link| link['href'] }
+
+    # binding.pry
+    article
+  end
+
+  def self.sub_article_3
+
+    doc = Nokogiri::HTML(open("https://bodybuilding.com"))
+
+    article = self.new
+    article.title = doc.search("#DPG_Workouts_Sub_3 .title").text.strip
+    article.description = doc.search("#DPG_Workouts_Sub_3 .description").text.strip
+    article.url = doc.search("#DPG_Workouts_Sub_3 a.thumb-container").map {|link| link['href'] }
+
+    # binding.pry
+    article
+  end
+
 
 end
