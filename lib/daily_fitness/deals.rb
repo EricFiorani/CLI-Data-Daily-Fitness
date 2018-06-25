bcc4745269e87bfcba6c1f5afe02b7b00a474ffa
Eric Fiorani <efiorani305@gmail.com>
2018-06-25 16:03:21 +0000

    comment adjustments

--- lib/daily_fitness/deals.rb
+++ lib/daily_fitness/deals.rb
@@ -5,13 +5,14 @@ class Deals
   @@all = []
 
   def initialize(deal_hash)
-    #creates a hash that organizes the keys and values of the information
-    #funneled in.
+    #creates a hash that organizes the keys and values of the newly created
+    #information funneled in via send.
     deal_hash.each {|k, v| self.send(("#{k}="), v)}
     @@all << self
   end
 
   def self.create_from_products(top_deals)
+    #takes in an array of hashes from the scraper
     #called through the CLI that prompts the initialization of a hash
     #(deal_hash)
     top_deals.each {|deal_hash| Deals.new(deal_hash)}
